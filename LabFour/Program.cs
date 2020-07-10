0dbad9ee30b77cf31af24865d67997a278dcbbfc
Ada <asmelliem@gmail.com>
2020-07-10 11:37:39 -0400

    Added complete code for Lab 4

--- LabFour/Program.cs
+++ LabFour/Program.cs
@@ -6,7 +6,40 @@ namespace LabFour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var proceed = "";
+            do
+            {
+                Console.WriteLine("Please enter an integer: ");
+                var userInput = Console.ReadLine();
+                var validInput = int.TryParse(userInput, out var number);
+
+                if (!validInput)
+                {
+                    Console.WriteLine("Whoops, looks like you typed in something that we can't process");
+                    Console.WriteLine("");
+                    proceed = "Y";
+                    continue;
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("{0,-9} {1,-9} {2,-9}", "Number", "Squared", "Cubed");
+                Console.WriteLine("{0,-9} {1,-9} {2,-9}", "======", "=======", "=====");
+
+                for (int currNum = 1; currNum <= number; currNum++)
+                {
+                    int square = currNum * currNum;
+                    int cubed = currNum * currNum * currNum;
+                    Console.WriteLine("{0,-9} {1,-9} {2,-9}", currNum, square, cubed);
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Continue? (y/n)");
+                proceed = Console.ReadLine().ToUpper();
+                Console.WriteLine("");
+
+            } while (proceed == "Y");
+
+            
         }
     }
 }
