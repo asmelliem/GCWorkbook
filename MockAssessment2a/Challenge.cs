d229a35b79432ea289cc2a70f65d2653569f94bd
Ada <asmelliem@gmail.com>
2020-08-12 21:29:32 -0400

    Added mock assessment 2a

---
+++ MockAssessment2a/Challenge.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MockAssessment2a
+{
+    public class Challenge
+    {
+        public static int AddStarWarsCharacters(string [] characters)
+        {
+            int index = Array.FindIndex(characters, w => w.Contains("Yoda"));
+            return index;
+        }
+
+        public static string DeathStarCombat(Dictionary<string,int> characterStats)
+        {
+            string character = characterStats.OrderByDescending(x => x.Value).First().Key;
+            return character;
+        }
+
+        public static List<string> ConvertPlanets(string [] planets)
+        {
+            List<string> reversedPlanets = new List<string>();
+            for (int i = planets.Length-1; i >= 0; i--)
+            {
+                reversedPlanets.Add(planets[i]);
+            }
+            return reversedPlanets;
+        }
+
+        public static double AverageDroids(List<int> droids)
+        {
+            List<int> evenNumberedDroids = new List<int>();
+            foreach (var droid in droids)
+            {
+                if(droid%2 == 0)
+                {
+                    evenNumberedDroids.Add(droid);
+                }
+            }
+
+            double sum = 0;
+            foreach (var evenDroid in evenNumberedDroids)
+            {
+                sum += evenDroid;
+            }
+
+            return sum / evenNumberedDroids.Count();
+        }
+
+        public static string TryToCatchDarthVadar(string sentence)
+        {
+            try
+            {
+                int.Parse(sentence);
+                return "Vadar Was Captured!";
+
+            }
+            catch(FormatException)
+            {
+                return "Vadar Got Away!";
+            }
+        }
+    }
+}
---
+++ MockAssessment2a/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockAssessment2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region #1
+            string[] characters = { "Obi-Wan", "Anikan", "Yoda", "Palpatine", "Luke" };
+            Console.WriteLine($"Yoda is found in index {Challenge.AddStarWarsCharacters(characters)}");
+            Console.WriteLine("");
+            #endregion
+
+            #region #2
+            Dictionary<string, int> characterStats = new Dictionary<string, int>();
+            characterStats.Add("Obi-Wan", 20);
+            characterStats.Add("Anikan", 30);
+            characterStats.Add("Yoda", 25);
+            characterStats.Add("Palpatine", 23);
+            characterStats.Add("Luke", 35);
+            Console.WriteLine($"The character with the highest attack power is {Challenge.DeathStarCombat(characterStats)}");
+            Console.WriteLine("");
+            #endregion
+
+            #region #3
+            string[] planets = { "Mercury", "Earth", "Venus", "Jupitar", "Mars", "Neptune", "Saturn", "Uranus" };
+            var listPlanets = Challenge.ConvertPlanets(planets);
+            foreach(var planet in listPlanets)
+            {
+                Console.Write($"{planet}, ");
+            }
+            Console.WriteLine("\n\n");
+            #endregion
+
+            #region #4
+            List<int> droids = new List<int>() { 4, 7, 2, 9, 10, 22, 53, 17, 94 , 63};
+            Console.WriteLine($"The average for the even numbered droid is {Challenge.AverageDroids(droids)}");
+            Console.WriteLine("");
+            #endregion
+
+            Console.WriteLine($"Successful Parse: {Challenge.TryToCatchDarthVadar("1")}");
+            Console.WriteLine($"Unsuccessful Parse: {Challenge.TryToCatchDarthVadar("hello")}");
+
+        }
+    }
+}
